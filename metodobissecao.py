d9db152c9f2ad60d6548d0be14b4ca731705b7c6
cromasc <joao_marcos_calixto@hotmail.com>
2022-10-22 15:05:54 -0300

    corrigindo ifs

--- metodobissecao.py
+++ metodobissecao.py
@@ -1,6 +1,6 @@
 from numpy import mean, e
 
-def _f(x): return e**(-2*x) - x
+def _f(x): return (e**(-2*x)) - x
 
 def bissecao(intervalo, f):
     if f(intervalo[0])*f(intervalo[1]) > 0: return "Não existe raíz no intervalo"
@@ -8,15 +8,24 @@ def bissecao(intervalo, f):
     i = 0
     while intervalo[1] - intervalo[0] > 10**-5:
         i += 1
-        xbarra = round(mean(intervalo),6)
 
-        if f(xbarra) < 0:
+        xbarra = round(mean(intervalo),6)
+        # fa = f(intervalo[0])
+        # fb = f(intervalo[1])
+        # fxbarra = f(xbarra)
+        # erro = abs(intervalo[1] - intervalo[0])
+        ...
+        if f(xbarra) < 0 and f(intervalo[0]) < 0:
             intervalo[0] = xbarra
-        elif f(xbarra) > 0:
+        elif f(xbarra) < 0 and f(intervalo[0]) > 0:
             intervalo[1] = xbarra
+        elif f(xbarra) > 0 and f(intervalo[1]) > 0:
+            intervalo[1] = xbarra
+        elif f(xbarra) > 0 and f(intervalo[1]) < 0:
+            intervalo[0] = xbarra
         else:
             break
-
+    ...
     return mean(intervalo), i
 
-print(bissecao([-1,0.5], _f))
+print(bissecao([0,0.5], _f))
