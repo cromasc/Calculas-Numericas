6ffc110c766f06ab6b213fd5c113ae27c3dd0cba
cromasc <joao_marcos_calixto@hotmail.com>
2022-11-07 21:53:08 -0300

    GAUSS-SEIDEL

--- sistemas lineares/gauss-seidel.py
+++ sistemas lineares/gauss-seidel.py
@@ -1,9 +1,3 @@
-# sistema = [
-#     [1,1,3,5],
-#     [1,3,1,6],
-#     [1,3,6,1]
-# ]
-
 def x1(x2,x3): return round(1 - x2/10 + x3/10,6)
 def x2(x1,x3): return round(1.2 - x1/10 - x3/10,6)
 def x3(x1,x2): return round(1.1 - x1*0.2 + x2/10,6)
@@ -11,12 +5,14 @@ def x3(x1,x2): return round(1.1 - x1*0.2 + x2/10,6)
 def erro(xk0: list, xk1: list): return [abs(xk1[i]-xk0[i]) for i in range(len(xk0))]
 
 def iteracao(E, xk0 = [0,0,0], k = -1):
+
+    a = x1(xk0[1], xk0[2])
+    b = x2(a, xk0[2])
+    c = x3(a, b)
     
-    xk1 = [x1(xk0[1], xk0[2]), x2(xk0[0], xk0[2]), x3(xk0[0], xk0[1])]
+    xk1 = [a, b, c]
     k += 1
 
     return (xk1,k) if max(erro(xk0,xk1)) < E else iteracao(E, xk1, k)
 
 print(iteracao(10**-2))
-
-# a = [round(x1(a[1], a[2]),6), round(x2(a[0], a[2]),6), round(x3(a[0], a[1]),6)]
